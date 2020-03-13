50ce2d2c4203abcbcd943199d7c8e3770a47841f
Lean <sanqq5908@gmail.com>
2020-03-13 17:02:25 +0800

    Initial Commit

---
+++ lesson2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson2
+//
+//  Created by lean on 2020/3/13.
+//  Copyright © 2020 lean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
